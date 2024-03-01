7168dd363b941a536e88bc446ec0ab98811527e9
missycoder mylan <noreen.taha@outlook.com>
2024-03-01 12:02:46 +0000

    create navbar and JS intro

---
+++ js-intro.js
@@ -0,0 +1,33 @@
+function sayHello() {
+    console.log("Hello World!"); //statement
+}
+
+sayHello(); //camelCase
+
+// Comments => `shift` + `//`
+// single-line comments => `(ctrl + /)`
+/** This 
+is a multi-line
+ comment
+ => press `forward-slash(/)` + `**` */
+/* or press
+`shift` + `alt` + `A`
+ */
+
+
+// VARIABLES
+let number = 18;
+// e.g. if we want to change the value of 18,
+number = 23
+// it can change to 23
+console.log(number); //changeable or mutable
+
+// CONST - constant or unchangeable
+const name = "Noreen";
+name = "missy";
+console.log(name); //THIS WON'T WORK, AN ERROR WILL APPEAR IN THE CONSOLE
+
+
+
+
+
