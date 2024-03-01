01d64ad2854f58801f500166e9b35716c4bf0dfa
missycoder mylan <noreen.taha@outlook.com>
2024-03-01 12:36:06 +0000

    Added Data Types

---
+++ Day6/data-types.js
@@ -0,0 +1,32 @@
+console.log("Data Types");
+
+// Data Types - Primitive
+// 1. numbers - whole, decimals, neg 
+let age = 25;
+console.log("Age: " + age);
+console.log(typeof age);    // print out as 'number' in console
+
+// 2. strings = ' ' or  " "
+let name = "Tony";
+console.log(typeof name);   // print out as 'string' in console
+
+// 3. boolean
+let isStudent = true;
+console.log(isStudent);     // print out as 'true' in console
+
+// undefined - uninitialized or missing value
+// example 1
+// let score;
+// console.log(typeof score);  // print out as `undefined` in console.THIS WON'T WORK
+
+//example 2
+let score;
+score = 20;
+console.log(typeof score);  // print out as `number` in console
+
+//null - intentional absence of any object value
+let car = null;
+console.log(typeof car);    // print out as 'object` in console
+
+
+
