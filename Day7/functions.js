f2c1b0096cbd85c6f8c8d80a705c5bb2e05a3d37
missycoder mylan <noreen.taha@outlook.com>
2024-03-04 13:30:37 +0000

    Added functions

--- Day7/functions.js
+++ Day7/functions.js
@@ -1,4 +1,4 @@
-// functions
+// FUNCTIONS
 // Defining a function
 function greet() {
     console.log("Hello, welcome to the world of functions!");
@@ -22,4 +22,18 @@ const squareExpression = function(num) {
 console.log("Square for function declaration: ", squareDeclaration(5));
 console.log("Square for function expression: ", squareExpression(6));
 
+
+// FUNCTION HOISTING
+// only work for function declaration
+// This WON'T WORK because it cannot access our function because we are already calling it
+sayHello()
+
+function sayHello() {
+    console.log("Hello, function hoisting!")
+};
+
+helloAnonymous()
+const helloAnonymous = function() {
+    console.log("Hoisting")        
+};
 
