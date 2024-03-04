1d930be8f6e1e702a218216b9a00fa45c4bce196
missycoder mylan <noreen.taha@outlook.com>
2024-03-04 12:34:51 +0000

    Added common array methods

--- Day7/arrays.js
+++ Day7/arrays.js
@@ -42,3 +42,32 @@ let menu = ['tajine', 'couscous', 'harira', 'khubz'];
 menu.forEach(function (menu) {
 console.log('Menu: ', menu);
 })
+
+// COMMON ARRAY METHODS
+// `colors.length` will print out `4` in console
+console.log("Array Length: ", colors.length);
+
+// indexOf 
+console.log("Index of Green: ", colors.indexOf('green'));
+
+// includes
+// `includes` will print out `true` or `false`
+// caseSensitive
+// `yellow` print out as `true`; `Yellow` print out as `false`
+console.log("Includes Yellow: ", colors.includes('yellow'));
+
+// join (comma, dash, asterisk, /, etc)
+console.log("Joined Array: ", colors.join('*'));
+
+// slice
+// slice accepts 2 arguments
+// only index 1 and 2 will print out in console
+let slicedArray = colors.slice(1, 3);
+console.log("Sliced Array: ", slicedArray);
+console.log("Colors Array: ", colors);
+
+// splice
+// only index 1 and 3 will print out in console
+let removedElements = colors.splice(1, 2);
+console.log("Spliced Array: ", removedElements);
+console.log("New Colors array: ", colors);
