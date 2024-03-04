ef488849db209ff29074e444ae15928b4935a322
missycoder mylan <noreen.taha@outlook.com>
2024-03-04 11:27:31 +0000

    Added Loops

---
+++ Day7/loop.js
@@ -0,0 +1,22 @@
+// FOR LOOP
+// i++ is an increment operator
+// this loop will print out `0 to 9` in console
+for (let i = 0; i < 10; i++ ) {
+    console.log("For loop: ", i);
+}
+
+// WHILE LOOP
+// this while loop will print out `count:1 to count:5` in console
+let count = 1
+while(count <= 5) {
+    console.log("count: ", count);
+count++;
+}
+
+// DO-WHILE LOOPS
+// this `do-while loop` will print out `do-while counter:1 to do-while counter:5` in console
+let i = 7;
+do {
+console.log("Do-while counter: ", i);
+i++;
+} while (i <=5);
