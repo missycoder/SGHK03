ee120e6e6510e617d8e95492ef549cf49f10aeb2
missycoder mylan <noreen.taha@outlook.com>
2024-03-08 12:18:19 +0000

    DOM Manipulation

---
+++ Day9/dom.js
@@ -0,0 +1,42 @@
+// Accessing elements
+// by classname
+const heading1 = document.getElementsByClassName("heading1");
+console.log(heading1);
+
+// by ID
+const heading = document.getElementById("heading");
+console.log(heading);
+
+// by tagName
+const heading2 = document.getElementsByTagName("h2");
+console.log(heading2);
+
+// by querySelector
+const listItems = document.querySelectorAll("#list li");
+console.log(listItems);
+
+// Modifying Elements
+// innerHTML
+heading1[0].innerHTML = "<h6>Learning DOM Manipulation</h6>";
+
+// textContent
+heading2[0].textContent = "Element Manipulation";
+
+// setting attributes
+heading1[0].setAttribute("class", "highlight");
+
+// changing style property
+const button = document.getElementById("btn");
+button.style.backgroundColor = "skyblue";
+
+// creating elements
+const container = document.getElementsByClassName("container");
+const newParagraph = document.createElement("p");
+newParagraph.textContent = "This is a new paragraph";
+
+container[0].appendChild(newParagraph);
+
+// removing an element
+const elementToRemove = document.querySelector("li");
+elementToRemove.remove()
+console.log("Removed Elements:", elementToRemove);
